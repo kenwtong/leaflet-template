7237ea762351a7833486ff9de6abce6e09ed9ef1
Ken Tong <kenwtong@gmail.com>
2015-06-09 23:33:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Leaflet Template
+
+A webmap template using the [Leaflet](http://leafletjs.com) mapping JavasScript library and [Bootstrap](http://getbootstrap.com).
+
+The template is preloaded with layers from:
+- [OpenStreetMap](https://www.openstreetmap.org)
+- [Stamen](http://stamen.com)
+- [Esri](http://esri.com)
+
+## Usage
+- Download the files
+- Remove the base layers you don't need
+- Add the layers important to you
+- Change the About and Contact modals
+- Change the title
+- Deploy
---
+++ js/main.js
@@ -0,0 +1,35 @@
+var map = L.map('map').setView([51.050, -114.067], 10);
+
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+var tonerLayer = new L.StamenTileLayer("toner");
+var topographicLayer = L.esri.basemapLayer("Topographic");
+var grayLayer = L.esri.basemapLayer("Gray");
+var grayLabelsLayer = L.esri.basemapLayer("GrayLabels");
+//var imageryLayer = L.esri.basemapLayer("Imagery");
+//var imageryLabelsLayer = L.esri.basemapLayer("ImageryLabels");
+//var imageryTransportationLayer = L.esri.basemapLayer("ImageryTransportation");
+var oceansLayer = L.esri.basemapLayer("Oceans");
+var oceansLabelLayer = L.esri.basemapLayer("OceansLabels");
+
+/*
+L.marker([51.050, -114.067]).addTo(map)
+    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+    .openPopup();
+*/
+
+// Group tiles and labels together for Esri tiles
+var oceansGroupLayer = L.layerGroup([oceansLayer, oceansLabelLayer]);
+var grayGroupLayer = L.layerGroup([grayLayer, grayLabelsLayer]);
+//var imageryGroupLayer = L.layerGroup([imageryLayer, imageryLabelsLayer, imageryTransportationLayer]);
+
+var baseMaps = {
+	"Gray": grayGroupLayer,
+	"Oceans": oceansGroupLayer,
+	"OpenStreetMap": osmLayer,
+    "Toner": tonerLayer,
+	"Topographic": topographicLayer
+};
+
+L.control.layers(baseMaps).addTo(map);
